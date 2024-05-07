67eb099b5c99651a4b85074fb3e04c8b8d93ae78
CCVpopular <datkhodo888@gmail.com>
2024-05-07 14:03:16 +0700

    v11

--- Areas/Admin/Controllers/ProductManagerController.cs
+++ Areas/Admin/Controllers/ProductManagerController.cs
@@ -56,15 +56,16 @@ namespace ShopGiay.Areas.Admin.Controllers
                     // Lưu hình ảnh đại diện
                     product.ImageUrl = await SaveImage(imageUrl);
                 }
-                //if (imageUrls != null)
-                //{
-                //    product.ImageUrls = new List<string>();
-                //    foreach (var file in imageUrls)
-                //    {
-                //        // Lưu các hình ảnh khác
-                //        product.ImageUrls.Add(await SaveImage(file));
-                //    }
-                //}
+                if (imageUrls != null && imageUrls.Count > 0)
+                {
+                    product.Images = new List<ProductImage>();
+                    foreach (var file in imageUrls)
+                    {
+                        // Lưu các hình ảnh khác
+                        var imageUrlPD = await SaveImagePD(file,product.Name);
+                        product.Images.Add(new ProductImage { Url = imageUrlPD });
+                    }
+                }
                 await _productRepository.AddAsync(product);
                 return RedirectToAction(nameof(Add));
             }
@@ -97,8 +98,43 @@ namespace ShopGiay.Areas.Admin.Controllers
                     System.IO.File.Delete(imagePath);
                 }
             }
-            
 
+        }
+
+        private async Task<string> SaveImagePD(IFormFile image, string productName)
+        {
+            var productImagePath = Path.Combine("wwwroot/images/DescriptionProduct", productName);
+
+            // Tạo thư mục nếu nó không tồn tại
+            if (!Directory.Exists(productImagePath))
+            {
+                Directory.CreateDirectory(productImagePath);
+            }
+
+            var savePath = Path.Combine(productImagePath, image.FileName);
+
+            using (var fileStream = new FileStream(savePath, FileMode.Create))
+            {
+                await image.CopyToAsync(fileStream);
+            }
+
+            return "/images/DescriptionProduct/" + productName + "/" + image.FileName;
+        }
+
+
+        private async Task DeleteImagePD(ProductImage? productImages)
+        {
+            if (productImages != null)
+            {
+                // xóa các hình ảnh 
+                var imagePath = Path.Combine("wwwroot", productImages.Url.TrimStart('/'));
+
+                if (System.IO.File.Exists(imagePath))
+                {
+                    // Xóa tệp ảnh nếu nó tồn tại
+                    System.IO.File.Delete(imagePath);
+                }
+            }
         }
 
         // Hiển thị form cập nhật sản phẩm
@@ -115,25 +151,55 @@ namespace ShopGiay.Areas.Admin.Controllers
         }
         // Xử lý cập nhật sản phẩm
         [HttpPost]
-        public async Task<IActionResult> Update(int id, Product product, IFormFile imageUrl, List<IFormFile> imageUrls)
+        public async Task<IActionResult> Update(int id, Product product, IFormFile imageUrl, List<IFormFile> imageUrls, List<int> deletedImageIds)
         {
             if (id != product.Id)
             {
                 return NotFound();
             }
-            if (ModelState.IsValid)
+            var productimage = await _productRepository.GetByIdAsync(id);
+            if (imageUrl != null)
             {
-                var productimage = await _productRepository.GetByIdAsync(id);
-                await DeleteImage(productimage.ImageUrl);
-
-                // Lưu hình ảnh mới và cập nhật URL hình ảnh trong sản phẩm
-                product.ImageUrl = await SaveImage(imageUrl);
-                await _productRepository.UpdateAsync(product);
-
-
-                return RedirectToAction(nameof(Index));
+                if(productimage.ImageUrl != null)
+                {
+                    await DeleteImage(productimage.ImageUrl);
+                    // Lưu hình ảnh mới và cập nhật URL hình ảnh trong sản phẩm
+                    product.ImageUrl = await SaveImage(imageUrl);
+                }
             }
-            return View(product);
+            else
+            {
+                product.ImageUrl = productimage.ImageUrl;
+            }
+
+            if (deletedImageIds != null && deletedImageIds.Any())
+            {
+                foreach (var deletedImageId in deletedImageIds)
+                {
+                    if (productimage.Images != null)
+                    {
+                        var deletedImage = productimage.Images.FirstOrDefault(img => img.Id == deletedImageId);
+                        if (deletedImage != null)
+                        {
+                            await DeleteImagePD(deletedImage); // Viết hàm xóa hình ảnh
+                            await _productRepository.DeleteImageAsync(deletedImageId);
+                        }
+                    }
+                }
+            }
+
+            if (imageUrls != null && imageUrls.Any())
+            {
+                foreach (var image in imageUrls)
+                {
+                    var imageUrlAdd = await SaveImagePD(image, product.Name); // Viết hàm lưu hình ảnh
+                    // Tạo mới ProductImage và thêm vào danh sách hình ảnh của sản phẩm
+                    await _productRepository.AddImageAsync(new ProductImage { Url = imageUrlAdd ,ProductId = id });
+                }
+            }
+
+            await _productRepository.UpdateAsync(product);
+            return RedirectToAction(nameof(Index));
         }
         // Hiển thị form xác nhận xóa sản phẩm
         public async Task<IActionResult> Delete(int id)
@@ -151,6 +217,14 @@ namespace ShopGiay.Areas.Admin.Controllers
         {
             var product = await _productRepository.GetByIdAsync(id);
             await DeleteImage(product.ImageUrl);
+
+            if (product.Images != null && product.Images.Count > 0)
+            {
+                foreach (var file in product.Images)
+                {
+                    await DeleteImagePD(file);
+                }
+            }
             await _productRepository.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
         }
